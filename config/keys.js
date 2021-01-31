babf324d3021e87969d727f4328e325e577d1545
Nide17 <nidedrogba@gmail.com>
2021-01-31 15:18:03 +0200

    changed the mongodb cluster

--- config/keys.js
+++ config/keys.js
@@ -1,4 +1,5 @@
 module.exports = {
     //  mongoURI:'mongodb://localhost:27017/workersdb'
-        mongoURI: 'mongodb+srv://parmenide:30uYAg3b8K9Vzdat@cluster0-cz2dw.mongodb.net/test?retryWrites=true&w=majority'
+    // mongoURI: 'mongodb+srv://parmenide:30uYAg3b8K9Vzdat@cluster0-cz2dw.mongodb.net/test?retryWrites=true&w=majority'
+    mongoURI: 'mongodb+srv://parmenide:jesus123@cluster0.u1ey7.mongodb.net/mern_workers?retryWrites=true&w=majority'
 }
